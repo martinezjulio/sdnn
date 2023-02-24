06a513ed586e4b0eb4c4b1b43f3076a9eb583134
El4h <67368850+El4h@users.noreply.github.com>
2023-02-24 05:55:32 +0100

    Update train.sh
    modified to run on the JLU server

--- training/train.sh
+++ training/train.sh
@@ -20,29 +20,20 @@
 
 #SBATCH --output='./output/train/%A_%a.out'
 
-#CONFIG_FILE='./configs/alexnet/face_inanimate_400k_nobatchnorm.yaml'
-#CONFIG_FILE='./configs/alexnet/face_inanimate_400k_orig.yaml'
-#CONFIG_FILE='./configs/vgg/face_inanimate_400k_seed2.yaml'
-#CONFIG_FILE='./configs/vgg/face_casia_100k_seed1.yaml'
-#CONFIG_FILE='./configs/vgg/face_AFD_full_seed.yaml'
-#CONFIG_FILE='./configs/vgg/face_na_seed.yaml'
-CONFIG_FILE='./configs/vgg/face_AFD_matched_seed.yaml'
-#CONFIG_FILE='./configs/vgg/cars_seed.yaml'
-#CONFIG_FILE='./configs/resnet/face_400k_resnet50.yaml'
-#CONFIG_FILE='./configs/resnet/inanimate_400k_resnet50.yaml'
-#CONFIG_FILE='./configs/alexnet/face_400k_nobatchnorm.yaml'
-#CONFIG_FILE='./configs/alexnet/inanimate_400k_nobatchnorm.yaml'
+CONFIG_FILE='./configs/vgg/face_dual_whitasia.yaml'
 
-SCRIPT=./train_new.py
+
+SCRIPT=./train.py
 
 hostname
 date
-echo "Sourcing conda..."
-source /mindhive/nklab4/users/kdobs/anaconda3/bin/activate
+echo "Sourcing python..."
+source /shared/venvs/py3.8-torch1.7.1/bin/activate
 date
-echo "Activating conda env..."
+echo "Activating required packages..."
 #conda activate test
-conda activate torch-gpu-dev
+#conda activate torch-gpu-dev
+pip install -r requirements.txt
 date
 echo "Running python script..."
 CUDA_VISIBLE_DEVICES=0 python $SCRIPT --config_file $CONFIG_FILE --num_epochs 201 --read_seed 1 --maxout True --save_freq 10 --valid_freq 1 --use_scheduler "True" --pretrained "False" # --custom_learning_rate 0.0001
